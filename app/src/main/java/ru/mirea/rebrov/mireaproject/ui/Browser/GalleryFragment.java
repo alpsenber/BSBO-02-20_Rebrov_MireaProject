6cf45d179e9a0e611bf5e8382a305037128abd5f
AsdFgh <alpsen.ber@gmail.com>
2023-04-05 19:00:30 +0300

    MireaProject

---
+++ app/src/main/java/ru/mirea/rebrov/mireaproject/ui/Browser/GalleryFragment.java
@@ -0,0 +1,42 @@
+package ru.mirea.rebrov.mireaproject.ui.Browser;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import ru.mirea.rebrov.mireaproject.databinding.FragmentGalleryBinding;
+
+public class GalleryFragment extends Fragment {
+
+    private FragmentGalleryBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        GalleryViewModel galleryViewModel =
+                new ViewModelProvider(this).get(GalleryViewModel.class);
+
+        binding = FragmentGalleryBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        WebView webObject = binding.mywebview;
+        webObject.loadUrl("https://ru.wikipedia.org/wiki");
+        WebSettings webParametrs = webObject.getSettings();
+        webParametrs.setJavaScriptEnabled(true);
+        webObject.setWebViewClient(new WebViewClient());
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
