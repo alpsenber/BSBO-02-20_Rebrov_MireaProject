5786a393da8518a4f0e47a27cd4f76c05e173bdf
AsdFgh <alpsen.ber@gmail.com>
2023-04-26 18:18:53 +0300

    MireaProject

--- app/src/main/java/ru/mirea/rebrov/mireaproject/MainActivity.java
+++ app/src/main/java/ru/mirea/rebrov/mireaproject/MainActivity.java
@@ -1,5 +1,9 @@
 package ru.mirea.rebrov.mireaproject;
 
+import static android.Manifest.permission.FOREGROUND_SERVICE;
+import static android.Manifest.permission.POST_NOTIFICATIONS;
+
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -7,6 +11,8 @@ import android.view.Menu;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -27,14 +33,15 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        if (ContextCompat.checkSelfPermission(this, POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{POST_NOTIFICATIONS, FOREGROUND_SERVICE}, 200);
+        }
 
         setSupportActionBar(binding.appBarMain.toolbar);
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_musicPlayerFragment)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
---
+++ app/src/main/java/ru/mirea/rebrov/mireaproject/MusicPlayerFragment.java
@@ -0,0 +1,56 @@
+package ru.mirea.rebrov.mireaproject;
+
+import static android.Manifest.permission.FOREGROUND_SERVICE;
+import static android.Manifest.permission.POST_NOTIFICATIONS;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ru.mirea.rebrov.mireaproject.databinding.FragmentMusicPlayerBinding;
+
+
+public class MusicPlayerFragment extends Fragment {
+
+    private FragmentMusicPlayerBinding binding;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentMusicPlayerBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        if (ContextCompat.checkSelfPermission(root.getContext(), POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+
+            Log.d(MainActivity.class.getSimpleName().toString(), "Разрешения получены");
+        } else {
+            Log.d(MainActivity.class.getSimpleName().toString(), "Нет разрешений!");
+
+            ActivityCompat.requestPermissions(getActivity(), new String[]{POST_NOTIFICATIONS, FOREGROUND_SERVICE}, 200);
+
+        }
+        binding.imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent serviceIntent = new Intent(root.getContext(), MusicService.class);
+                ContextCompat.startForegroundService(root.getContext(), serviceIntent);
+            }
+        });
+        binding.imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                root.getContext().stopService(new Intent(root.getContext(), MusicService.class));
+            }
+        });
+        return root;
+    }
+}
